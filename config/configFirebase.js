ccc949201fb286e89793281f08c607f82a70193c
Syuhada Rantisi <suadarantisi@gmail.com>
2023-07-22 23:33:30 +0700

    Add vercel config for service accounts

--- config/configFirebase.js
+++ config/configFirebase.js
@@ -1,6 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app')
-const serviceAccount =
-    process.env.FIREBASE_SECRET || require('./ServiceAccount.json')
+let serviceAccount =  require('./vercelConfig') 
+if (serviceAccount.private_key === undefined) serviceAccount = require('./ServiceAccount.json')
+
 const app = () =>  initializeApp({ credential: cert(serviceAccount) })
 
 module.exports = app
