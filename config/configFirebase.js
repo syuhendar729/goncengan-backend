a12bbc7bc7bab529264b779592d02a98a2c474d0
Syuhada Rantisi <suadarantisi@gmail.com>
2023-07-22 22:54:11 +0700

    Try delete ServiceAccount.json

--- config/configFirebase.js
+++ config/configFirebase.js
@@ -1,6 +1,6 @@
 const { initializeApp, cert } = require('firebase-admin/app')
 const serviceAccount =
     process.env.FIREBASE_SECRET || require('./ServiceAccount.json')
-const app = () => initializeApp({ credential: cert(serviceAccount) })
+const app = () =>  initializeApp({ credential: cert(serviceAccount) })
 
 module.exports = app
